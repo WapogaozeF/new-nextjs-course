b97d0b00793da4c1e80fc61b2b2ba22f15343269
Verner Ravil <werner.ravil@gmail.com>
2025-04-04 11:53:35 +0500

    Added alt to Image component on [mealSlug] page

--- foodies/app/meals/[mealSlug]/page.js
+++ foodies/app/meals/[mealSlug]/page.js
@@ -16,7 +16,7 @@ export default function MealDetailsPage({ params }) {
 		<>
 			<header className={classes.header}>
 				<div className={classes.image}>
-					<Image src={meal.image} fill />
+					<Image src={meal.image} alt={meal.title} fill />
 				</div>
 				<div className={classes.headerText}>
 					<h1>{meal.title}</h1>
