e55119f7a6a78b042f67d9a7837f9d8340f61bc9
Verner Ravil <werner.ravil@gmail.com>
2025-04-13 23:49:18 +0500

    Added useFormStatus

--- mutation-data/app/new-post/page.js
+++ mutation-data/app/new-post/page.js
@@ -1,4 +1,6 @@
+import FormSubmit from '@/components/form-submit';
 import { storePost } from '@/lib/posts';
+import { redirect } from 'next/navigation';
 
 export default function NewPostPage() {
   async function createPost(formData) {
@@ -7,12 +9,14 @@ export default function NewPostPage() {
     const image = formData.get('image');
     const content = formData.get('content');
 
-    storePost({
+    await storePost({
       imageUrl: '',
       title,
       content,
       userId: 1
-    })
+    });
+
+    redirect("/feed");
   }
 
   return (
@@ -21,7 +25,7 @@ export default function NewPostPage() {
       <form action={createPost}>
         <p className="form-control">
           <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+          <input type="text" id="title" name="title" required />
         </p>
         <p className="form-control">
           <label htmlFor="image">Image URL</label>
@@ -37,8 +41,7 @@ export default function NewPostPage() {
           <textarea id="content" name="content" rows="5" />
         </p>
         <p className="form-actions">
-          <button type="reset">Reset</button>
-          <button>Create Post</button>
+         <FormSubmit />
         </p>
       </form>
     </>
