9db076e392505cd2a054ee6b31462dd3c85906c0
Verner Ravil <werner.ravil@gmail.com>
2025-05-31 01:32:44 +0500

    Added programmaticaly navigate

--- pages-router/pages/clients/[id]/index.js
+++ pages-router/pages/clients/[id]/index.js
@@ -1,7 +1,18 @@
+import { useRouter } from "next/router";
+
 function ClientProjectsPage() {
+	const router = useRouter();
+
+	function loadProjectHandler() {
+		router.push("/clients/max/projecta");
+	}
+
 	return (
 		<div>
 			<h1>The Project of a Given Client</h1>
+			<button onClick={loadProjectHandler} type="button">
+				Load Project A
+			</button>
 		</div>
 	);
 }
--- pages-router/pages/clients/index.js
+++ pages-router/pages/clients/index.js
@@ -2,15 +2,15 @@ import Link from "next/link";
 
 function ClientsPage() {
 	const clients = [
-		{ id: "Max", name: "Maximilian" },
-		{ id: "Manu", name: "Manuel" },
+		{ id: "max", name: "Maximilian" },
+		{ id: "manu", name: "Manuel" },
 	];
 
 	return (
 		<div>
 			<h1>The Clients Page</h1>
 			<ul>
-				{clients.map( (client) => (
+				{clients.map((client) => (
 					<li key={client.id}>
 						<Link href={`/clients/${client.id}`}>{client.name}</Link>
 					</li>
