81ef2007f305367d60c8fc45f3cc65f1576615b0
Verner Ravil <werner.ravil@gmail.com>
2025-05-31 22:13:15 +0500

    Added Client Side data loading via SWR

--- data-fetching/pages/_app.js
+++ data-fetching/pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { SWRConfig } from "swr";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<SWRConfig
+			value={{
+				refreshInterval: 3000,
+				fetcher: (resource, init) =>
+					fetch(resource, init).then((res) => res.json()),
+			}}
+		>
+			<Component {...pageProps} />
+		</SWRConfig>
+	);
 }
 
-export default MyApp
+export default MyApp;
