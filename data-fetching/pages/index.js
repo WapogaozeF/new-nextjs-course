36e629483fc336f51ea38711d1575e09c516e307
Verner Ravil <werner.ravil@gmail.com>
2025-05-31 11:27:07 +0500

    Added loading data from backend

--- data-fetching/pages/index.js
+++ data-fetching/pages/index.js
@@ -1,11 +1,28 @@
-function HomePage(props) {
-  return (
-    <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
-    </ul>
-  );
+import fs from "node:fs";
+import path from "node:path";
+import util from "node:util";
+
+function HomePage({ products }) {
+	return (
+		<ul>
+			{products.map((product) => (
+				<li key={product.id}>{product.title}</li>
+			))}
+		</ul>
+	);
+}
+
+export async function getStaticProps() {
+	const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+	const promisifyReadFile = util.promisify(fs.readFile);
+	const jsonData = await promisifyReadFile(filePath);
+	const data = JSON.parse(jsonData);
+
+	return {
+		props: {
+			products: data.products,
+		},
+	};
 }
 
 export default HomePage;
