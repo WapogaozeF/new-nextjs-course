4f30b39726a45365ca747665e33cba6306e38adb
Verner Ravil <werner.ravil@gmail.com>
2025-06-04 14:48:29 +0500

    Added Sign up notification

--- react-context/pages/_app.js
+++ react-context/pages/_app.js
@@ -18,7 +18,6 @@ function MyApp({ Component, pageProps }) {
 					/>
 				</Head>
 				<Component {...pageProps} />
-				<Notification title="Test" message="This is a test." status="pending" />
 			</Layout>
 		</NotificationContextProvider>
 	);
