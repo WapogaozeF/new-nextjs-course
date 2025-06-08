a8f1ea3b5b9d4ef581fe2472ee4ec1620969a000
Verner Ravil <werner.ravil@gmail.com>
2025-06-08 23:38:19 +0500

    Downgrade NextAuthand add default auth

--- pages-router-authentication/components/layout/main-navigation.js
+++ pages-router-authentication/components/layout/main-navigation.js
@@ -1,28 +1,33 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useSession } from "next-auth/client";
 
-import classes from './main-navigation.module.css';
+import classes from "./main-navigation.module.css";
 
 function MainNavigation() {
-  return (
-    <header className={classes.header}>
-      <Link href="/">
-        <div className={classes.logo}>Next Auth</div>
-      </Link>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/auth">Login</Link>
-          </li>
-          <li>
-            <Link href="/profile">Profile</Link>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  );
+	const [session, loading] = useSession();
+	console.log(session, loading);
+	return (
+		<header className={classes.header}>
+			<Link href="/">
+				<div className={classes.logo}>Next Auth</div>
+			</Link>
+			<nav>
+				<ul>
+					<li>
+						<Link href="/auth">Login</Link>
+					</li>
+					{session && (
+						<li>
+							<Link href="/profile">Profile</Link>
+						</li>
+					)}
+					<li>
+						<button type="button">Logout</button>
+					</li>
+				</ul>
+			</nav>
+		</header>
+	);
 }
 
 export default MainNavigation;
---
+++ pages-router-authentication/lib/db.js
@@ -0,0 +1,5 @@
+import { MongoClient } from "mongodb";
+
+export async function connectToDatabase() {
+	return await MongoClient.connect("mongodb://localhost:27017/auth");
+}
